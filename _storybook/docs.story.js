459e37f855eabd1530528a88cc12e37cfd037096
Peter Goes <petergoes@gmail.com>
2018-04-17 13:35:23 +0200

    chore: rename docs/ to _docs/

--- _storybook/docs.story.js
+++ _storybook/docs.story.js
@@ -2,7 +2,7 @@ import startCase from 'lodash/startCase'
 import { storiesOf } from '@storybook/vue'
 
 const stories = storiesOf('Documentation', module)
-const docs = require.context('../docs', true, /.md$/)
+const docs = require.context('../_docs', true, /.md$/)
 
 docs.keys().forEach(filename => {
   const [, , docTitle] = filename.match(/(\.\/)([\w-]+)/)
