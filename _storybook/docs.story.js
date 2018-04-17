ccb77551ecb69a20581b5ec85ad80f9db0700d33
Peter Goes <petergoes@gmail.com>
2018-04-17 17:14:19 +0200

    refactor(docs.story): add gfm to documentation

--- _storybook/docs.story.js
+++ _storybook/docs.story.js
@@ -1,13 +1,30 @@
 import startCase from 'lodash/startCase'
 import { storiesOf } from '@storybook/vue'
+import README from '../README.md'
 
 const stories = storiesOf('Documentation', module)
 const docs = require.context('../_docs', true, /.md$/)
+const gfmCSS =
+  'https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.css'
+const wrapperStyling =
+  'box-sizing: border-box;min-width: 200px;max-width: 980px;margin: 0 auto;padding: 45px;'
+
+stories.add('README', () => ({
+  template: `
+    <div class="markdown-body" style="${wrapperStyling}">
+      <link rel="stylesheet" href="${gfmCSS}"/>
+      ${README}
+    </div>`,
+}))
 
 docs.keys().forEach(filename => {
   const [, , docTitle] = filename.match(/(\.\/)([\w-]+)/)
 
   stories.add(startCase(docTitle), () => ({
-    template: `<div>${docs(filename)}</div>`,
+    template: `
+      <div class="markdown-body" style="${wrapperStyling}">
+        <link rel="stylesheet" href="${gfmCSS}"/>
+        ${docs(filename)}
+      </div>`,
   }))
 })
